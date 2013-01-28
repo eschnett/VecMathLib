1652c0d29e23f46ca349f13b93ef272f2ca03aa4
Erik Schnetter <schnetter@gmail.com>
2013-01-28 13:10:37 -0500

    Allow round-off in sin()

--- mathfuncs_sin.h
+++ mathfuncs_sin.h
@@ -15,18 +15,20 @@ namespace vecmathlib {
   template<typename realvec_t>
   realvec_t mathfuncs<realvec_t>::vml_sin(realvec_t x)
   {
+    typename realvec_t::scalar_t eps = 0.000001;
+    
     // Reduce range: sin(x) = sin(x + 2pi)
     x = remainder(x, RV(2.0*M_PI));
-    assert(all(x >= RV(-M_PI) && x <= RV(M_PI)));
+    assert(all(x >= RV(-(1.0+eps)*M_PI) && x <= RV((1.0+eps)*M_PI)));
     
     // Reduce range: sin(x) = -sin(-x)
     realvec_t sign = x;
     x = fabs(x);
-    assert(all(x >= RV(0.0) && x <= RV(M_PI)));
+    assert(all(x >= RV(0.0) && x <= RV((1.0+eps)*M_PI)));
     
     // Reduce range: sin(x) = sin(pi-x)
     x = ifthen(x > RV(M_PI_2), RV(M_PI) - x, x);
-    assert(all(x >= RV(0.0) && x <= RV(M_PI_2)));
+    assert(all(x >= RV(0.0-eps) && x <= RV(M_PI_2)));
     
     // Reduce range: cos(x) = sin(pi/2 - x)
     boolvec_t use_cos = x > RV(0.5*M_PI_2);
@@ -40,12 +42,15 @@ namespace vecmathlib {
     realvec_t x2 = x*x;
     realvec_t y = ifthen(use_cos, RV(1.0), x);
     realvec_t r = y;
+    // realvec_t rn = RV(FP::convert_float(3)) + noffset;
+    // realvec_t drn = RV(FP::convert_float(2));
     for (int n=3; n<nmax; n+=2) {
       // sin: (n-1)*n
       // cos: n*(n+1)
       realvec_t rn = RV(FP::convert_float(n)) + noffset;
       y *= - x2 * rcp((rn-RV(1.0))*rn);
       r += y;
+      // rn += drn;
     }
     
     // Undo range reduction
