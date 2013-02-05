714b247452161b6f54e4190d64a3e211a05c682f
Erik Schnetter <schnetter@gmail.com>
2013-02-05 13:11:28 -0500

    Correct termination condition in mask class

--- vec_mask.h
+++ vec_mask.h
@@ -8,22 +8,25 @@
 
 
 namespace vecmathlib {
+  using namespace std;
   
   template<typename realvec_t>
   class mask_t {
+    
     friend realvec_t;
+    
     typedef typename realvec_t::boolvec_t boolvec_t;
     typedef typename realvec_t::intvec_t intvec_t;
     static int const size = realvec_t::size;
     
-    std::ptrdiff_t imin, imax;
-    std::ptrdiff_t i;
+    ptrdiff_t imin, imax;
+    ptrdiff_t i;
     boolvec_t m;
     bool all_m;
     
   public:
     mask_t(boolvec_t m_): m(m_), all_m(all(m)) {}
-    mask_t(std::ptrdiff_t imin_, std::ptrdiff_t imax_, std::ptrdiff_t ioff):
+    mask_t(ptrdiff_t imin_, ptrdiff_t imax_, ptrdiff_t ioff):
       imin(imin_), imax(imax_),
       i(imin - (ioff + imin) % size)
     {
@@ -35,8 +38,8 @@ namespace vecmathlib {
              intvec_t(i) <= intvec_t(imax-size) - intvec_t::iota());
       }
     }
-    std::ptrdiff_t index() const { return i; }
-    operator bool() const { return i >= imax; }
+    ptrdiff_t index() const { return i; }
+    operator bool() const { return i<imax; }
     void operator++()
     {
       i += size;
