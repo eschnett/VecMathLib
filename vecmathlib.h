7956991ebe4bc605fb70de798f9c8786634f0bd1
Erik Schnetter <schnetter@gmail.com>
2013-02-04 23:41:44 -0500

    Add flag VML_DEBUG
    Also define VML_ASSERT for error-checking

--- vecmathlib.h
+++ vecmathlib.h
@@ -3,10 +3,22 @@
 #ifndef VECMATHLIB_H
 #define VECMATHLIB_H
 
-// #define VML_HAVE_DENORMALS
-// #define VML_HAVE_INF
-// #define VML_HAVE_NAN
-// #define VML_HAVE_SIGNED_ZERO
+#define VML_DEBUG
+
+// #undef VML_HAVE_DENORMALS
+// #undef VML_HAVE_INF
+// #undef VML_HAVE_NAN
+// #undef VML_HAVE_SIGNED_ZERO
+
+
+
+#include <cassert>
+
+#ifdef VML_DEBUG
+#  define VML_ASSERT(x) assert(x)
+#else
+#  define VML_ASSERT(x) ((void)0)
+#endif
 
 #include "vec_pseudo.h"
 
