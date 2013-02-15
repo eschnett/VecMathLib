bca784f977b600c2e6db057961a92af2eac2bd3c
Erik Schnetter <schnetter@gmail.com>
2013-02-15 09:57:04 -0500

    Look at exp() code

--- instantiations.cc
+++ instantiations.cc
@@ -11,16 +11,19 @@ using namespace std;
 namespace vecmathlib {
   
 #ifdef VECMATHLIB_HAVE_VEC_DOUBLE_1
+  template realvec<double,1> exp(realvec<double,1> x);
   template realvec<double,1> sin(realvec<double,1> x);
   template realvec<double,1> sqrt(realvec<double,1> x);
 #endif
   
 #ifdef VECMATHLIB_HAVE_VEC_DOUBLE_2
+  template realvec<double,2> exp(realvec<double,2> x);
   template realvec<double,2> sin(realvec<double,2> x);
   template realvec<double,2> sqrt(realvec<double,2> x);
 #endif
   
 #ifdef VECMATHLIB_HAVE_VEC_DOUBLE_4
+  template realvec<double,4> exp(realvec<double,4> x);
   template realvec<double,4> sin(realvec<double,4> x);
   template realvec<double,4> sqrt(realvec<double,4> x);
 #endif
