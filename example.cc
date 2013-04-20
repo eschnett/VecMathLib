184dfda46b02e09b6fd52e8ef0d063fad684fe2a
Erik Schnetter <schnetter@gmail.com>
2013-04-20 11:10:16 -0400

    Be flexible about available vector types in example.cc

--- example.cc
+++ example.cc
@@ -5,14 +5,21 @@
 #include <iostream>
 
 using namespace std;
+using namespace vecmathlib;
 
 
 
 int main(int argc, char** argv)
 {
-  using namespace vecmathlib;
-  // typedef realvec<float,1> realvec_t;
+  // Choose an "interesting" vector type
+#if defined VECMATHLIB_HAVE_VEC_DOUBLE_2
   typedef realvec<double,2> realvec_t;
+#elif defined VECMATHLIB_HAVE_VEC_FLOAT_4
+  typedef realvec<float,4> realvec_t;
+#else
+  typedef realvec<float,1> realvec_t;
+#endif
+
   typedef realvec_t::boolvec_t boolvec_t;
   typedef realvec_t::intvec_t intvec_t;
   
