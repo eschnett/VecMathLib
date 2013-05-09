9bad5e405719fdd84f052a3de54b3572ebbb46e8
Erik Schnetter <schnetter@gmail.com>
2013-05-09 15:53:32 -0400

    Correct isnan() for SSE2 double

--- vec_double_sse2_scalar.h
+++ vec_double_sse2_scalar.h
@@ -424,7 +424,7 @@ namespace vecmathlib {
     boolvec_t isinf() const { return std::isinf(v); }
     boolvec_t isnan() const
     {
-      return _mm_ucomineq_ss(from_double(v), from_double(v));
+      return _mm_ucomineq_sd(from_double(v), from_double(v));
     }
     boolvec_t isnormal() const { return std::isnormal(v); }
     realvec ldexp(int_t n) const { return std::ldexp(v, n); }
